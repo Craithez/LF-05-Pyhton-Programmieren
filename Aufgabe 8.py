43c409bfc37f993111f74367b5a0dfb22c62da7f
Marc <48025136+Craithez@users.noreply.github.com>
2023-12-13 13:28:42 +0100

    1
    Commit

---
+++ Aufgabe 8.py
@@ -0,0 +1,14 @@
+x = 7
+a = 11
+
+if(x < 9):print(bool(True))
+else:print(bool(False))
+if(x != 7):print(bool(True))
+else:print(bool(False))
+if(a > 11):print(bool(True))
+else:print(bool(False))
+if(a == 8):print(bool(True))
+else:print(bool(False))
+if(a <= 11):print(bool(True))
+else:print(bool(False))
+
---
+++ Aufgabe 9.py
@@ -0,0 +1,5 @@
+Geben Sie den booleschen Wert (True, False) für die folgenden Ausdrücke an, wenn x = 4 gilt.
+
+(x < 9) & (x >= -5) (x < 4) or (x <= 4)
+not(x < 5 and x < 10) (x*2 < 10) and (x+6 == 10)
+not(x) (x % 2 == 1) and (x % 3 == 1)
