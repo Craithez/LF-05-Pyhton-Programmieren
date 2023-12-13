da9de8066786056c8a6cdc2e94bcdb7a56019f1b
Marc <48025136+Craithez@users.noreply.github.com>
2023-12-13 13:36:00 +0100

    Update Aufgabe 2.py

--- Aufgabe 2.py
+++ Aufgabe 2.py
@@ -2,7 +2,7 @@
 stromKosten = 1467.29
 Grundgebühr = 132.90
 Hoch = 149.50
-## Durschnitt berechen
+## Durchschnitt berechen
 Durchschnitt = (stromKosten + Grundgebühr // 12)
 print("Durschnittliche Stromkosten pro Monat: ", Durchschnitt)
 if Durchschnitt > Hoch:print("Die Stromkosten von monatlich", Durchschnitt ,"€ sind zu hoch")
