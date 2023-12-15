aea649390d5db2908bae47c15afcc30602576101
Marc <48025136+Craithez@users.noreply.github.com>
2023-12-15 11:49:30 +0100

    Update

--- Aufgabe 3.py
+++ Aufgabe 3.py
@@ -1,4 +1,4 @@
-Zahl1 = float(input("Bitte Base eingeben!" ))
-Zahl2 = float(input ("Bitte Exponent eingeben! "))
-Zahl3 = Zahl1 ** Zahl2
-print("Ihre Zahl ist: ", Zahl3)
+Base = float(input("Bitte Base eingeben!" ))
+Exponent = float(input ("Bitte Exponent eingeben! "))
+Ergebnis = Base ** Exponent
+print("Ihre Zahl ist: ", Ergebnis)
--- Aufgabe 7.py
+++ Aufgabe 7.py
@@ -1,8 +1,6 @@
-
-
 zahl0 = int(122)
 zahl1 = int(47)
 zahl2 = int(255)
 zahl3 = int(1023)
 
-print(chr(zahl0), chr(zahl1), chr(zahl2), chr(zahl3))
+print(chr(zahl0), chr(zahl1), chr(zahl2), chr(zahl3)) 
