92dd521f68b7f50dd66b0f03a23c2b92674ef838
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-10 08:22:02 +0100

    commit

---
+++ Aufgabe 10.py
@@ -0,0 +1,10 @@
+#Aufgabe 10
+# x = x + 7 
+# a = a % 3
+x = int(input ("Bitte x eingeben! "))
+a = int(input ("Bitte a eingeben! "))
+x += 7
+a %= 3 
+
+print("x: ", x)
+print("a: ", a)
---
+++ Aufgabe 12.py
@@ -0,0 +1,12 @@
+#Aufgabe 12
+
+a. Warum kann kein einziger der Einwohner von sich behaupten: “Ich bin ein Schurke.”?
+Weil die Ritter nur die Warhheit sagen und die Schurken immer lügen.
+
+b. Ein Volkszähler befragt ein Ehepaar: “Wer von Ihnen ist Ritter, und wer ist Schurke?”
+Der Mann antwortet: “Wir sind beide Schurken.”.
+Der Mann ist Schurke, die Frau ist Ritter.
+
+c. Ein Volkszähler befragt ein Ehepaar: “Wer von Ihnen ist Ritter und wer ist Schurke?”.
+Der Mann antwortet: “Mindestens einer von uns ist Schurke.”
+Der Mann ist Ritter, die Frau Schurke.
