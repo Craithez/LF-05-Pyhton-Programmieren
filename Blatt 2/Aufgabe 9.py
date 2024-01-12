d88245659a3505bab88e9fb59c5337fc8b88d80c
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-12 10:55:00 +0100

    Create Aufgabe 9.py

---
+++ Blatt 2/Aufgabe 9.py
@@ -0,0 +1,21 @@
+def sortieren(array):
+	n = len(array)
+
+	swapped = False
+	for i in range(n-1):
+
+		for j in range(0, n-i-1):
+			if array[j] > array[j + 1]:
+				swapped = True
+				array[j], array[j + 1] = array[j + 1], array[j]
+		
+		if not swapped:
+			return
+
+array = [64, 34, 25, 12, 22, 11, 90]
+
+sortieren(array)
+
+print("Sortierte Daten:")
+for i in range(len(array)):
+	print("% d" % array[i], end=" ")
