32f074d48b2e0e653c930ba388c5a80cea548043
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-12 08:41:55 +0100

    update

---
+++ Blatt 2/Aufgabe 5.py
@@ -0,0 +1,20 @@
+
+
+Skizzieren Sie den entsprechenden Programmablaufplan bevor Sie sich dem Code zuwenden.
+Schreiben Sie den Code. Die Durchschnittstemperatur soll auf zwei Stellen gerundet werden.
+Die Temperaturwerte sollen nicht in drei Variablen gespeichert werden, sondern in einer Liste.
+
+abfrage = str(input("Wollen sie eine Temperatur eingeben (bitte mit j oder n antworten): "))
+    if abfrage == j:
+    temp = float(input("Temperatur angeben (in °): "))
+    for temp
+    if temp <= 30:
+        print("Temperatur is normal!")
+    elif temp > 30 & < 40:
+        print("Temperatur ist höher als normal!")
+    elif temp >= 40:
+        print("Temperatur ist zu hoch!")
+    else:
+        print("Gib ordentliche Werte an!")
+    else:
+    print("Fertig.")
---
+++ Blatt 2/Aufgabe 6.py
@@ -0,0 +1,40 @@
+#ip = str(input("IP eingeben : "))
+#octets = 0
+#octets == ip
+#number = 0
+#number == ip
+#def check_ip(ip):
+#        octets = ip.split(".")
+#        if len(octets)!=4:
+#            print("Keine richtige IP Adresse!")
+#            return "Ungültige IP"
+#for number in octets:
+#    if not number==str:
+#        print("Keine richtige IP Adresse!")
+#    if str(number)<0 or int(number)>255:
+#        print("Keine richtige IP Adresse!")
+#    else:
+#        print("IP Adresse ist gültig!")
+def validIP(inp):
+    if inp.find('/') == -1:
+        return False
+    ips,mask = inp.split('/')
+    if not mask.isdigit():
+        return False
+    if ips.isnumeric()>255:
+        return False
+    if ips.isnumeric()<0:
+        return False
+    if ips.count('.') > 3:
+        return False
+    for ip in ips.split('.'):
+        if not ip.isdigit():
+            return False
+    return True
+
+print("Starte!")
+IPv4 = ''
+while not validIP(IPv4):
+    IPv4 =input("Bitte gültige IP Adresse eingeben (#.#.#.#:####):")
+    print(IPv4)
+print ("valid:", IPv4)
