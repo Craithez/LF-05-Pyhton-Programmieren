9ce70f21f5843dd63609fcd7f9d997c4b49af047
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-16 11:56:54 +0100

    Update

--- Blatt 2/Aufgabe 12.py
+++ Blatt 2/Aufgabe 12.py
@@ -1,15 +1,18 @@
 note = int(input("Note eingeben! "))
-if note == 1:
-    print("Sehr gut!")
-elif note == 2:
-    print("Gut!")
-elif note == 3:
-    print("Befriedigend!")
-elif note == 4:
-    print("Ausreichend!")
-elif note == 5:
-    print("Mangelhaft!")
-elif note == 6:
-    print("Ungenügend!")
-else:
-    print("Ungültige Eingabe!")    
+match note:
+    case 1 if note == 1:
+        print("Sehr gut!")
+    case 2 if note == 2:
+        print("Gut!")
+    case 3 if note == 3:
+        print("Befriedigend!")
+    case 4 if note == 4:
+        print("Ausreichend!")
+    case 5 if note == 5:
+        print("Mangelhaft!")
+    case 6 if note == 6:
+        print("Ungenügend!")
+    case 7 if note <=0:
+        print("Ungültige Eingabe")
+    case 8 if note >= 7:
+        print("Ungültige Eingabe")
--- Blatt 2/Aufgabe 14.py
+++ Blatt 2/Aufgabe 14.py
@@ -1,21 +1,20 @@
 geschwindigkeitsLimit = float(input("Geschwindkeitslimit: "))
 geschwindigkeit = float(input("Geschwindigkeit: "))
-unterschied = 0
-if geschwindigkeit < geschwindigkeitsLimit:
+if geschwindigkeit <= geschwindigkeitsLimit:
     print("Passt, fahr schneller.")
     exit()
-elif geschwindigkeit < 100:
-    verändereGeschwindigkeit = geschwindigkeit -((geschwindigkeit/100)*3)
+
+if geschwindigkeit < 100:
+    geschwindigkeit = geschwindigkeit -((geschwindigkeit/100)*3)
 else:
-    verändereGeschwindigkeit = geschwindigkeit -((geschwindigkeit/100)*5) 
-    veränderung = geschwindigkeitsLimit - verändereGeschwindigkeit
-    if verändereGeschwindigkeit < 20:  
-        print("Glück gehabt! Du warst ", verändereGeschwindigkeit, "zu schnell")
-    elif verändereGeschwindigkeit >= 20:
-        print("Du warst ", verändereGeschwindigkeit, "zu schnell, 30 Euro.")
-    elif verändereGeschwindigkeit >= 30 and verändereGeschwindigkeit < 50:
-        print("Du warst ", verändereGeschwindigkeit, "zu schnell, 120 Euro.")
-    else:
-        print("Du warst VIEL", verändereGeschwindigkeit, "zu schnell, 240 Euro.")        
-    print("Glück gehabt!")
+    geschwindigkeit = geschwindigkeit -((geschwindigkeit/100)*5) 
+unterschied = geschwindigkeit - geschwindigkeitsLimit
+if unterschied < 20:  
+    print("Glück gehabt! Du warst nur ", unterschied, "zu schnell")
+elif unterschied >= 20:
+    print("Du warst ", unterschied, "zu schnell, 30 Euro.")
+elif unterschied >= 30 and unterschied < 50:
+    print("Du warst ", unterschied, "zu schnell, 120 Euro.")
+else:
+    print("Du warst VIEL", unterschied, "zu schnell, 240 Euro.")        
 
