23337ce9326df9ff1fa8500031f04397fa3a144f
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-18 09:53:08 +0100

    Updated

--- Blatt 2/Aufgabe 10.py
+++ Blatt 2/Aufgabe 10.py
@@ -1,25 +1,27 @@
 einheit = str(input("Bitte geben sie die Einheit an! In a(KB),b(MB) oder c(GB) an."))
-a = None
-b = None
-c = None
-match einheit:
-    case 1 if einheit == a:
-        dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
-        übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
-        transferZeit = None
-        transferZeit = (dateiGröße/1000)/übertragungsRate
-        print("Es dauert: ", transferZeit, "s")
-    case 2 if einheit == b:
-        dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
-        übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
-        transferZeit = None
-        transferZeit = dateiGröße/übertragungsRate
-        print("Es dauert: ", transferZeit, "s")
-    case 3 if einheit == c:       
-        dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
-        übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
-        transferZeit = None
-        transferZeit = (dateiGröße*1000)/übertragungsRate
-        print("Es dauert: ", transferZeit, "s")
-
+a = "KB"
+b = "MB"
+c = "GB"
+def einheitencheck(einheit):
+    match einheit:
+        case 1 if einheit == a:
+            dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
+            übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
+            transferZeit = None
+            transferZeit = (dateiGröße/1000)/übertragungsRate
+            print("Es dauert: ", transferZeit, "s")
+        case 2 if einheit == b:
+            dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
+            übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
+            transferZeit = None
+            transferZeit = dateiGröße/übertragungsRate
+            print("Es dauert: ", transferZeit, "s")
+        case 3 if einheit == c:       
+            dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
+            übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
+            transferZeit = None
+            transferZeit = (dateiGröße*1000)/übertragungsRate
+            print("Es dauert: ", transferZeit, "s")
+    print(transferZeit)
+#Funktioniert noch nicht
 
---
+++ Blatt 2/Aufgabe 13.py
@@ -0,0 +1,21 @@
+total = input("Einkaufswert eingeben: ")
+country = input(str("Zielland eingeben: "))
+shipping = None
+
+if country == "Japan":
+    if total <= 50:
+        shipping = 50
+    elif total <= 100:
+        shipping = 25
+    elif total >= 180:
+        shipping = 0
+    else:
+        shipping = 5
+elif country == "Australien":
+    if total <= 50:
+        shipping = 40
+    else:
+        shipping = 15
+print ("Ihre Kosten Lieferkosten betragen ",shipping,"$")
+
+
