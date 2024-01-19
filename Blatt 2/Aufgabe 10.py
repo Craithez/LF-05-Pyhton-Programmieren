24731bc2f8a22f97db7e7e2d1041d9699df87f5c
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-19 14:09:27 +0100

    Update Aufgabe 10.py

--- Blatt 2/Aufgabe 10.py
+++ Blatt 2/Aufgabe 10.py
@@ -1,27 +1,25 @@
-einheit = str(input("Bitte geben sie die Einheit an! In a(KB),b(MB) oder c(GB) an."))
 a = "KB"
 b = "MB"
 c = "GB"
-def einheitencheck(einheit):
-    match einheit:
-        case 1 if einheit == a:
-            dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
-            übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
-            transferZeit = None
-            transferZeit = (dateiGröße/1000)/übertragungsRate
-            print("Es dauert: ", transferZeit, "s")
-        case 2 if einheit == b:
-            dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
-            übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
-            transferZeit = None
-            transferZeit = dateiGröße/übertragungsRate
-            print("Es dauert: ", transferZeit, "s")
-        case 3 if einheit == c:       
-            dateiGröße = input("Bitte geben sie die Dateigröße an (in ihrer Einheit): ")
-            übertragungsRate = input("Bitte geben sie ihre Übertragungsrate an (in MB/s): ")
-            transferZeit = None
-            transferZeit = (dateiGröße*1000)/übertragungsRate
-            print("Es dauert: ", transferZeit, "s")
-    print(transferZeit)
-#Funktioniert noch nicht
 
+def einheitencheck(einheit):
+    if einheit == a:
+        dateiGröße = float(input("Bitte geben sie die Dateigröße an (in ihrer Einheit): "))
+        übertragungsRate = float(input("Bitte geben sie ihre Übertragungsrate an (in MB/s): "))
+        transferZeit = (dateiGröße / 1000) / übertragungsRate
+        print("Es dauert:", transferZeit, "s")
+    elif einheit == b:
+        dateiGröße = float(input("Bitte geben sie die Dateigröße an (in ihrer Einheit): "))
+        übertragungsRate = float(input("Bitte geben sie ihre Übertragungsrate an (in MB/s): "))
+        transferZeit = dateiGröße / übertragungsRate
+        print("Es dauert:", transferZeit, "s")
+    elif einheit == c:
+        dateiGröße = float(input("Bitte geben sie die Dateigröße an (in ihrer Einheit): "))
+        übertragungsRate = float(input("Bitte geben sie ihre Übertragungsrate an (in MB/s): "))
+        transferZeit = (dateiGröße * 1000) / übertragungsRate
+        print("Es dauert:", transferZeit, "s")
+    else:
+        print("Ungültige Einheit!")
+
+einheit = input("Bitte geben sie die Einheit an! In a(KB), b(MB) oder c(GB) an: ")
+einheitencheck(einheit)
