24b93a67a493b897c596ebfcfaf62e2b646c40f3
Marc <48025136+Craithez@users.noreply.github.com>
2024-01-30 11:33:23 +0100

    Update Aufgabe 14.py

--- Blatt 2/Aufgabe 14.py
+++ Blatt 2/Aufgabe 14.py
@@ -11,9 +11,9 @@ else:
 unterschied = geschwindigkeit - geschwindigkeitsLimit
 if unterschied < 20:  
     print("Glück gehabt! Du warst nur ", unterschied, "zu schnell")
-elif unterschied >= 20:
+elif unterschied > 20 and unterschied < 30:
     print("Du warst ", unterschied, "zu schnell, 30 Euro.")
-elif unterschied >= 30 and unterschied < 50:
+elif unterschied > 30 and unterschied < 50:
     print("Du warst ", unterschied, "zu schnell, 120 Euro.")
 else:
     print("Du warst VIEL", unterschied, "zu schnell, 240 Euro.")        
