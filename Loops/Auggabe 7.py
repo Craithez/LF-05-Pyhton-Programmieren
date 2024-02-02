8a554e7b7e2bf6a8991ec9c0da79f420f31a069c
Marc <48025136+Craithez@users.noreply.github.com>
2024-02-02 12:33:02 +0100

    Create Auggabe 7.py

---
+++ Loops/Auggabe 7.py
@@ -0,0 +1,22 @@
+
+     
+
+def bin_2_dez(num):
+
+    bits = list(str(num))
+    dez = 0
+    zaehler = 0
+
+    for i in reversed(bits):
+        #print(bits) für Stellen einzusehen
+        dez += 2**zaehler * int(i)
+        zaehler+=1
+    print(dez)
+
+if __name__ == '__main__':
+
+    # Binär Value
+    bin_val = int(input("Binaerzahl eingeben: "))
+     
+    # Funktion aufrufen
+    bin_2_dez(bin_val)
