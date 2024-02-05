a1cbc431bfb2466b411d503e068ae3cd541293b4
Marc <48025136+Craithez@users.noreply.github.com>
2024-02-05 15:03:16 +0100

    Update Aufgabe 8-Windows.py

---
+++ Loops/Aufgabe 8-Windows.py
@@ -0,0 +1,7 @@
+def getmac(interface):
+    try:
+        mac = open('/sys/class/net/'+interface+'/address').readline()
+    except:
+        mac = "00:00:00:00:00:00"
+    return mac[0:17]
+myMAC = getmac("wlan0")
