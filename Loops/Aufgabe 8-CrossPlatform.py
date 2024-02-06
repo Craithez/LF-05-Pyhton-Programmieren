39b34fb37043a989251204d3aa3d1a1939c91b96
Marc <48025136+Craithez@users.noreply.github.com>
2024-02-06 07:07:52 +0100

    Update Aufgabe 8-CrossPlatform.py

--- Loops/Aufgabe 8-CrossPlatform.py
+++ Loops/Aufgabe 8-CrossPlatform.py
@@ -1,6 +1,6 @@
 import psutil
-nics = psutil.net_if_addrs()
-mac_address = nics['Ethernet'][0].address
+hexset = psutil.net_if_addrs()
+mac_address = hexset['Ethernet'][0].address
 print(mac_address)
 
-#kA wieso er es sich so schwer macht, psutil ist auf Linux und Windows ergo das einfachste.
+#kA wieso er es sich so schwer macht, psutil ist auf Linux und Windows mit den gleichen Befehlen bedienbar.
